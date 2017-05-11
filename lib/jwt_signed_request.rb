418e05b861297e9dc81c0836ebcc7e7eb63422a1
Odin Dutton <odindutton@gmail.com>
2017-05-11 11:28:47 +1000

    Extract sign class

--- lib/jwt_signed_request.rb
+++ lib/jwt_signed_request.rb
@@ -1,5 +1,5 @@
 require 'jwt'
-require 'jwt_signed_request/claims'
+require 'jwt_signed_request/sign'
 require 'jwt_signed_request/verify'
 
 module JWTSignedRequest
@@ -11,25 +11,8 @@ module JWTSignedRequest
   JWTDecodeError = Class.new(UnauthorizedRequestError)
   RequestVerificationFailedError = Class.new(UnauthorizedRequestError)
 
-  def self.sign(method:, path:,
-                body: EMPTY_BODY, headers:,
-                secret_key:, algorithm: DEFAULT_ALGORITHM,
-                key_id: nil, issuer: nil,
-                additional_headers_to_sign: Claims::EMPTY_HEADERS)
-    additional_jwt_headers = key_id ? {kid: key_id} : {}
-    JWT.encode(
-      Claims.generate(
-        method: method,
-        path: path,
-        headers: headers,
-        body: body,
-        additional_headers_to_sign: additional_headers_to_sign,
-        issuer: issuer
-      ),
-      secret_key,
-      algorithm,
-      additional_jwt_headers
-    )
+  def self.sign(*args)
+    Sign.call(*args)
   end
 
   def self.verify(*args)
