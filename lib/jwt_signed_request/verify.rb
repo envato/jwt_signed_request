e7504e398f5431cea8af0977d3d776621119ef60
Odin Dutton <odindutton@gmail.com>
2017-05-11 15:58:07 +1000

    Make better use of private methods in verify

--- lib/jwt_signed_request/verify.rb
+++ lib/jwt_signed_request/verify.rb
@@ -6,6 +6,7 @@ module JWTSignedRequest
       new(*args).call
     end
 
+    # TODO: algorithm is deprecated and will be removed in future
     def initialize(request:, secret_key:, algorithm: nil, leeway: nil)
       @request = request
       @secret_key = secret_key
@@ -14,29 +15,12 @@ module JWTSignedRequest
     end
 
     def call
-      # TODO: algorithm is deprecated and will be removed in future
-      verify = true
-      options = {}
-      if leeway
-        # TODO: Once JWT v2.0.0 has been released, we should upgrade to it and start using `exp_leeway` instead
-        #  'leeway' will still work, but 'exp_leeway' is more explicit and is the documented way to do it.
-        #  see https://github.com/jwt/ruby-jwt/pull/187
-        options[:leeway] = leeway.to_i
-      end
-      jwt_token = Headers.fetch('Authorization', request)
-
       if jwt_token.nil?
         raise MissingAuthorizationHeaderError, "Missing Authorization header in the request"
       end
 
-      begin
-        claims = JWT.decode(jwt_token, secret_key, verify, options)[0]
-        unless verified_request?(request: request, claims: claims)
-          raise RequestVerificationFailedError, "Request failed verification"
-        end
-
-      rescue ::JWT::DecodeError => e
-        raise JWTDecodeError, e.message
+      unless verified_request?
+        raise RequestVerificationFailedError, "Request failed verification"
       end
     end
 
@@ -44,20 +28,44 @@ module JWTSignedRequest
 
     attr_reader :request, :secret_key, :algorithm, :leeway
 
-    def verified_request?(request:, claims:)
-      claims['method'].to_s.downcase == request.request_method.downcase &&
-        claims['path'] == request.fullpath &&
-        claims['body_sha'] == Digest::SHA256.hexdigest(request_body(request: request)) &&
-        verified_headers?(request: request, claims: claims)
+    def jwt_token
+      @jwt_token ||= Headers.fetch('Authorization', request)
     end
 
-    def request_body(request:)
+    def claims
+      @claims ||= begin
+        verify = true
+        options = {}
+
+        if leeway
+          # TODO: Once JWT v2.0.0 has been released, we should upgrade to it
+          # and start using `exp_leeway` instead 'leeway' will still work, but
+          # 'exp_leeway' is more explicit and is the documented way to do it.
+          #
+          # See https://github.com/jwt/ruby-jwt/pull/187.
+          options[:leeway] = leeway.to_i
+        end
+
+        JWT.decode(jwt_token, secret_key, verify, options)[0]
+      rescue ::JWT::DecodeError => e
+        raise JWTDecodeError, e.message
+      end
+    end
+
+    def verified_request?
+      claims['method'].to_s.downcase == request.request_method.downcase &&
+        claims['path'] == request.fullpath &&
+        claims['body_sha'] == Digest::SHA256.hexdigest(request_body) &&
+        verified_headers?
+    end
+
+    def request_body
       string = request.body.read
       request.body.rewind
       string
     end
 
-    def verified_headers?(request:, claims:)
+    def verified_headers?
       parsed_headers = begin
         JSON.parse(claims['headers'].to_s)
       rescue JSON::ParserError
